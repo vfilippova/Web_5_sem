78908b11190fbf253be630bc4a5de216c566597a
Victoria <89811956+vfilippova@users.noreply.github.com>
2023-12-12 10:37:21 +0300

    Update PageLoadTime.js

--- PageLoadTime.js
+++ PageLoadTime.js
@@ -1,25 +1,24 @@
-
 // IIFE (Immediately Invoked Function Expression)
 (function () {
     // Функция для измерения времени загрузки страницы
     function measurePageLoadTime() {
         // Время начала загрузки страницы
-        const startTime = performance.now();
+        const StartTime = performance.now();
         // Событие, которое срабатывает при полной загрузке страницы
         window.addEventListener('load', () => {
             // Время окончания загрузки страницы
-            const endTime = performance.now();
+            const EndTime = performance.now();
 
             // Вычисляем время загрузки
-            const loadTime = endTime - startTime;
+            const LoadTime = EndTime - StartTime;
 
-            const footer =document.getElementById('footer-id')
+            const footer = document.getElementById('footer-id')
             // Выводим статистическую информацию в консоль (или можно вставить в подвал страницы)
             //alert(`Время загрузки страницы: ${loadTime.toFixed(1)}`);
-           footer.innerHTML = `Время загрузки страницы: ${loadTime.toFixed(1)}`;
+            footer.innerHTML = `Время загрузки страницы: ${LoadTime.toFixed(1)}`;
         });
     }
 
     // Вызываем функцию измерения времени загрузки
-measurePageLoadTime();
+    measurePageLoadTime();
 })();
