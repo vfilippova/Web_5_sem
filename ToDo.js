a1ba809e0a8f85f66209fbaa3f62eb9e7bfb6b14
Victoria <89811956+vfilippova@users.noreply.github.com>
2023-12-15 14:12:37 +0300

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,41 +1,75 @@
-// Функция, которая будет выполнена при загрузке DOM-дерева
+//функция, которая будет выполнена при загрузке DOM-дерева
 function handleDOMContentLoaded() {
-    // Проверяем, есть ли значение 'add-name' в localStorage
-    // Если есть, устанавливаем значение поля ввода 'add-name' равным сохраненному значению
+    //проверяем, есть ли значение 'add-name' в localStorage
+    //если есть, устанавливаем значение поля ввода 'add-name' равным сохраненному значению
     if (localStorage.getItem('add-name')) {
         document.getElementById('add-name').value = localStorage.getItem('add-name');
     }
-    
-// Получаем элементы формы, поля ввода 'add-name' и контейнер таблицы
+    //получаем элементы формы, поля ввода 'add-name' и контейнер таблицы
     const form = document.getElementById('table-form');
     const addNameInput = document.getElementById('add-name');
     const tableContainer = document.getElementById('table-container');
-    
-    // Добавляем слушатель события 'submit' к форме
+
+    //функция для удаления карточки из контейнера таблицы
+    function removeCard(card) {
+        //удаляем данные о карточке из localStorage
+        localStorage.removeItem(card.id);
+        //удаляем карточку из контейнера таблицы
+        tableContainer.removeChild(card);
+    }
+
+    //добавляем слушатель события 'submit' к форме
     form.addEventListener('submit', function (e) {
         e.preventDefault();
-        
-        // Получаем значение поля ввода 'add-name'
+        //получаем значение поля ввода 'add-name'
         const addName = addNameInput.value;
-        
-        // Создаем новый элемент 'div' для карточки и добавляем ему класс 'card'
+        //создаем новый элемент 'div' для карточки и добавляем ему класс 'card'
         const card = document.createElement('div');
         card.classList.add('card');
-        
-        // Создаем новый элемент 'span' для текста карточки и задаем ему значение из поля ввода
+        //генерируем уникальный идентификатор для карточки
+        const cardId = 'card-' + Date.now();
+        card.id = cardId;
+        //создаем новый элемент 'span' для текста карточки и задаем ему значение из поля ввода
         const text = document.createElement('span');
         text.textContent = addName;
-        
-        // Добавляем элемент с текстом внутрь карточки
+        //добавляем элемент с текстом внутрь карточки
         card.appendChild(text);
-        
-        // Добавляем карточку в контейнер таблицы
+        //сохраняем карточку в localStorage с ключом, равным идентификатору
+        localStorage.setItem(cardId, addName);
+        //добавляем слушатель события 'click' к карточке
+        card.addEventListener('click', function () {
+            removeCard(card);
+        });
+        //добавляем карточку в контейнер таблицы
         tableContainer.appendChild(card);
-        
-// Сохраняем значение поля ввода в localStorage с ключом 'add-name'
+        //сохраняем значение поля ввода в localStorage с ключом 'add-name'
         localStorage.setItem('add-name', addName);
     });
+
+    //восстанавливаем сохраненные карточки при загрузке страницы
+    //цикл for перебирает элементы в локальном хранилище, используя свойство localStorage.length
+    for (let i = 0; i < localStorage.length; i++) {
+        //метод localStorage.key(i) используется для получения ключа каждого элемента
+        const key = localStorage.key(i);
+        //функция startsWith('card-') проверяет, начинается ли ключ со строки 'card-'.
+        if (key.startsWith('card-')) {
+            const cardId = key;
+            //метод localStorage.getItem(key) извлекает значение, связанное с ключом.
+            const addName = localStorage.getItem(key);
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.id = cardId;
+            const text = document.createElement('span');
+            text.textContent = addName;
+            card.appendChild(text);
+            //слушатель события 'click' добавляется к элементу карточки, чтобы при клике была вызвана функция removeCard(card).
+            card.addEventListener('click', function () {
+                removeCard(card);
+            });
+            tableContainer.appendChild(card);
+        }
+    }
 }
 
-// Добавляем слушатель события 'DOMContentLoaded' к объекту window и задаем ему функцию handleDOMContentLoaded
+//добавляем слушатель события 'DOMContentLoaded' к объекту window и задаем ему функцию handleDOMContentLoaded
 window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
